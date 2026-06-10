55aada9222f5ab9ad8a87881dcd908d644c3b189
Rushikesh Patare <rushikeshpatare4@gmail.com>
2026-06-10 14:30:50 +0530

    Time: 370 ms (17.40%) | Memory: 43.6 MB (6.18%) - LeetSync

---
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
@@ -0,0 +1,48 @@
+class Solution:
+    def generateValidStrings(self, n: int, k: int) -> list[str]:
+
+        arr = ["0"] * n 
+
+        ans = []
+        count = 0
+
+        def subseq(i , arr , count ):
+            if i >= n :
+                if count <= k :
+                    ans.append(arr.copy())
+                return 
+
+            arr[i] = "1"
+            count += i 
+            subseq(i+1 , arr, count )
+            arr[i] = "0"
+            count -= i 
+            subseq(i+1 , arr, count)
+
+        subseq(0 , arr , count)
+
+
+
+        finalseb = []
+        for i in ans:
+            flag = True
+            for x in range(len(i)-1):
+                if i[x] == '1' and i[x+1] == '1' :
+                    flag = False;break
+
+       
+            if flag :
+                finalseb.append("".join(i))
+        return finalseb
+
+
+
+
+
+        
+
+
+
+
+        
+        
