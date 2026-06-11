ac9a19e70567d1d53159316cb203a2dc678f62e0
Rushikesh Patare <rushikeshpatare4@gmail.com>
2026-06-11 08:38:35 +0530

    Time: 34 ms (65.49%) | Memory: 25.8 MB (43.19%) - LeetSync

--- 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
@@ -6,42 +6,23 @@ class Solution:
         def subseq(i , arr , count):
             if i >= n :
                 if count <= k :
-                    ans.append(arr.copy())
-                return 
-            if i < n-1 and arr[i] == arr[i+1] and arr[i] == "1":
+                    ans.append("".join(arr))
                 return 
 
-            arr[i] = "1"
-    
-            count += i 
+            arr[i] = '0'
+  
+
 
             subseq(i+1 , arr, count )
-            arr[i] = "0"
-            count -= i 
-            subseq(i+1 , arr, count)
+            
+            if i==0 or arr[i-1] == '0':
+                arr[i] = '1'
+                count += i
+                subseq(i+1 , arr, count)
+                arr[i] = '0'
 
         subseq(0 , arr , count = 0)
 
-        finalseb = []
+        return ans
 
-        for i in ans:
-            flag = True
-            for x in range(len(i)-1):
-                if i[x] == '1' and i[x+1] == '1' :
-                    flag = False;break
-
-            if flag :
-                finalseb.append("".join(i))
-        return finalseb
-
-
-
-
-
-        
-
-
-
-
-        
-        
+    
